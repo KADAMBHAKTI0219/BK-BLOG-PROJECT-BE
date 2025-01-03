c7b3555e33a4c62ae1e2d9c40545fc8ae06df1a3
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-01-03 20:02:11 +0530

    updated cors

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const dotenv = require('dotenv')
 const connectionToDB = require('./Config/db')
 const BlogPostsRouters = require('./Routes/BlogPostsRoutes')
 dotenv.config()
+const cors = require("cors")
 const app = express()
 app.use(express.json())
-
+app.use(cors(),{
+   origin: [" http://localhost:5173/"]
+})
 app.use('/posts',BlogPostsRouters)
 
 app.listen(process.env.PORT || 3000,async()=>{
