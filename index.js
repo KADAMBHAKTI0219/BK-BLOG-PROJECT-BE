af0c91bb8337c4e3837b59d0861c217a41ad4484
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-01-03 22:08:19 +0530

    completed

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectionToDB = require('./Config/db')
 const BlogPostsRouters = require('./Routes/BlogPostsRoutes')
-dotenv.config()
 const cors = require("cors")
+dotenv.config()
+
 const app = express()
 app.use(express.json())
-app.use(cors(),{
-   origin: [" http://localhost:5173/"]
-})
+app.use(cors({
+   origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"],
+   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+   credentials: true
+}))
 app.use('/posts',BlogPostsRouters)
 
 app.listen(process.env.PORT || 3000,async()=>{
